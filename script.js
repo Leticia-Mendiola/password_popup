a9921964bad4acb589a5e3097cdab08c0e836841
Leticia-Mendiola <lmendiola509@gmail.com>
2021-02-10 13:46:33 -0600

    fixed password generator code in javascript file

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = "";
-  var passwordText = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"+
-  "abcdefghijklmnopqrstuvwxyz!@#$%^&*";
-  for (i = 1; i <= 8; i++) { 
-    var char = Math.floor(Math.random() 
-                * passwordText.length + 1); 
-      
-    password += passwordText.charAt(char) 
-} 
-return password;
-}
+function writePassword(){
+  let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*";
+  let password = "";
+  for (var i = 0; i <= 8; i++) { 
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  } 
+
 
 // passwordText.value = password;
 
 // function gfg_Run(){generateBtn.innerHTML = fucntion writePassword()}
 // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword());
+
+document.getElementById("password").value = password;
+
+}
